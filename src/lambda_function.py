c9242cceae629a2921c88373772b8abcf88227e2
yotam <yotam.aflalo433@gmail.com>
2024-10-13 17:26:01 +0300

    fix

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    #we added sns :)  
+    #we added sns :) and fix it
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda CICD example repo!')
