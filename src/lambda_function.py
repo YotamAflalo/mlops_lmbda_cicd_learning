bd7142c281dbfb985f70c64cdd8bb889e3138f13
yotam <yotam.aflalo433@gmail.com>
2024-10-13 17:16:10 +0300

    "adding sns"

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,6 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    #we added sns :) 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda CICD example repo!')
